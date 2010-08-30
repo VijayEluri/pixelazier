8873bc493dc598b9b8e7878b10e497b0ce902841
Alexander Pupeikis <alex@apleben.com>
2010-08-31 01:59:58 +0400

    added utils package with common utils class implementation

---
+++ src/com/apleben/utils/common/Utils.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2010. Alexander Pupeikis
+ *
+ * This file is part of JavaPixelazier.
+ *
+ * JavaPixelazier is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JavaPixelazier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JavaPixelazier.  If not, see http://www.gnu.org/licenses/.
+ */
+
+package com.apleben.utils.common;
+
+import javax.swing.*;
+
+/**
+ * @author apupeikis
+ */
+public final class Utils {
+    private Utils() {
+        throw new AssertionError();
+    }
+
+    protected static ImageIcon createImageIcon(String path, String description) {
+        java.net.URL imgURL = Utils.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, description);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            System.err.println("With description: " + description);
+            return null;
+        }
+    }
+}
