020888c4553cf0d5f958cffc6a02c82d3bac0ca6
Alexander Pupeikis <alex@apleben.com>
2010-08-31 07:21:49 +0400

    added file icon view and image previewer into the swing common package for shared use through project.

---
+++ src/com/apleben/swing/common/FileIconView.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2010. Alexander Pupeikis
+ *
+ * This file is part of JavaPixelazier.
+ *
+ * JavaPixelazier is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JavaPixelazier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JavaPixelazier.  If not, see http://www.gnu.org/licenses/.
+ */
+
+package com.apleben.swing.common;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileView;
+import java.io.File;
+
+/**
+ * A file view that displays an icon for all files that match a file filter.
+ *
+ * @author apupeikis
+ */
+class FileIconView extends FileView {
+    /**
+     * Constructs a FileIconView.
+     *
+     * @param aFilter     a file filter--all files that this filter accepts will be shown with the icon.
+     * @param anIcon--the icon shown with all accepted files.
+     */
+    public FileIconView(FileFilter aFilter, Icon anIcon) {
+        filter = aFilter;
+        icon = anIcon;
+    }
+
+    @Override
+    public Icon getIcon(File f) {
+        if (!f.isDirectory() && filter.accept(f)) return icon;
+        else return null;
+    }
+
+    private FileFilter filter;
+    private Icon icon;
+}
---
+++ src/com/apleben/swing/common/ImagePreviewer.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2010. Alexander Pupeikis
+ *
+ * This file is part of JavaPixelazier.
+ *
+ * JavaPixelazier is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JavaPixelazier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JavaPixelazier.  If not, see http://www.gnu.org/licenses/.
+ */
+
+package com.apleben.swing.common;
+
+import javax.swing.*;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+
+/**
+ * A file chooser accessory that previews images.
+ *
+ * @author apupeikis
+ */
+class ImagePreviewer extends JLabel {
+    /**
+     * Constructs an ImagePreviewer.
+     *
+     * @param chooser the file chooser whose property changes trigger an image change in this
+     *                previewer
+     */
+    public ImagePreviewer(JFileChooser chooser) {
+        setPreferredSize(new Dimension(100, 100));
+        setBorder(BorderFactory.createEtchedBorder());
+
+        chooser.addPropertyChangeListener(new PropertyChangeListener() {
+            public void propertyChange(PropertyChangeEvent event) {
+                if (event.getPropertyName().equals(JFileChooser.SELECTED_FILE_CHANGED_PROPERTY)) {
+                    // the user has selected a new file
+                    File f = (File) event.getNewValue();
+                    if (f == null) {
+                        setIcon(null);
+                        return;
+                    }
+
+                    // read the image into an icon
+                    ImageIcon icon = new ImageIcon(f.getPath());
+
+                    // if the icon is too large to fit, scale it
+                    if (icon.getIconWidth() > getWidth()) icon = new ImageIcon(icon.getImage()
+                            .getScaledInstance(getWidth(), -1, Image.SCALE_DEFAULT));
+
+                    setIcon(icon);
+                }
+            }
+        });
+    }
+}
